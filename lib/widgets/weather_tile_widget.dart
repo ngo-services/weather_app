c32bdd9e74a00759d8fbffc8e952ccc3a394fb4d
ngo-services <113435432+ngo-services@users.noreply.github.com>
2023-08-31 12:30:36 +0300

    Add files via upload

---
+++ lib/widgets/weather_tile_widget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class WeatherTileWidget extends StatelessWidget {
+  BuildContext? context;
+  String? title;
+  double? titleFontSize;
+  String? subTitle;
+  WeatherTileWidget({
+    Key? key,
+    this.context,
+    this.title,
+    this.titleFontSize,
+    this.subTitle,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Row(
+            children: [
+              const Icon(
+                Icons.location_on_outlined,
+                color: Colors.white,
+              ),
+              const SizedBox(width: 4),
+              Text(
+                title!,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: titleFontSize,
+                  fontWeight: FontWeight.w500,
+                  fontFamily: 'Roboto',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
